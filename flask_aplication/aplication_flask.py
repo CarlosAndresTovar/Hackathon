21d8fb984c10ee8342b041c7a4cfe91a36798883
CarlosAndresTovar <carlos.andres.tovarr@gmail.com>
2024-07-20 15:58:47 -0500

    Creación aplicación flask

--- flask_aplication/aplication_flask.py
+++ flask_aplication/aplication_flask.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__) # Habilitar el modo de depuración
 
 @app.route('/')
-def hello():
-    return "Hello, World!"
+def index():
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
